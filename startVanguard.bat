ca17cbeb92b41aea3876957a73106bb604db1d07
OwlH4x <60191004+oOInfodevOo@users.noreply.github.com>
2024-04-30 06:08:55 -0700

    Update startVanguard.bat
    removed vgtray

--- startVanguard.bat
+++ startVanguard.bat
@@ -2,7 +2,6 @@
 net start vgc
 if ERRORLEVEL 1 goto error
 Start "" "C:\Program Files\Riot Vanguard\vgc.exe"
-Start "" "C:\Program Files\Riot Vanguard\vgtray.exe"
 exit
 :error
 echo Could not start Vanguard
